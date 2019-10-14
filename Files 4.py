8b44b806e77d90555447816966e89faeaad3df00
kornevvi <vlad.kornev@live.ru>
2019-10-14 18:13:54 +0500

    Работа с файлами задание №1

--- Files 4.py
+++ Files 4.py
@@ -1,4 +1,23 @@
-with open ('file.txt') as inf:
-    t = inf.readline()
-    t1 = t[1]
-    print(t1.isdigit())
+with open('file.txt','r') as inf:
+    s = inf.readline()
+sym = ''
+num = ''
+res = ''
+resfull = ''
+for i in range(0,len(s)-1):
+    if s[i].isalpha():
+        sym = s[i]
+        i += 1
+    else:
+        if s[i+1].isdigit():
+            num = s[i]+s[i+1]
+            i += 2
+            res = sym * int(num)
+        else:
+            num = s[i]
+            i += 1
+            res = sym * int(num)
+    resfull += res
+#with open('file.txt','w') as ouf:
+#    ouf.write(resfull)
+print(resfull)
