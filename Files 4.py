77254ab402b9112ee9faf37c38f32aead75ffa00
kornevvi <vlad.kornev@live.ru>
2019-10-15 16:50:09 +0500

    Добавлено решение первой задачи на в-в файлов

--- Files 4.py
+++ Files 4.py
@@ -1,23 +1,32 @@
 with open('file.txt','r') as inf:
-    s = inf.readline()
-sym = ''
-num = ''
-res = ''
-resfull = ''
-for i in range(0,len(s)-1):
+    s = inf.readline().strip()
+sym = '' # переменная в которой будет хранится последний считанный символ
+num = '' # переменная в которой будет хранится число повторений символа
+res = '' # результат одной итерации цикла
+ires = ''# итоговая распакованная строка
+i = 0
+while i <= len(s) - 1:
+    # проверка, является ли символ буквой
     if s[i].isalpha():
         sym = s[i]
-        i += 1
+        res = s[i]
+        st = 1
     else:
-        if s[i+1].isdigit():
-            num = s[i]+s[i+1]
-            i += 2
-            res = sym * int(num)
+        # проверка, является ли количесво повторений буквы двухзначным числом
+        if (i+1) <= (len(s)-1): # дополнительная проверка на случай ошибки IndexError, когда проверяется [i + 1] символ строки
+            if s[i+1].isdigit():
+                num = s[i] + s[i+1]
+                res = sym * (int(num) - 1)
+                st = 2
+            else:
+                num = s[i]
+                res = sym * (int(num) - 1)
+                st = 1
         else:
             num = s[i]
-            i += 1
-            res = sym * int(num)
-    resfull += res
-#with open('file.txt','w') as ouf:
-#    ouf.write(resfull)
-print(resfull)
+            res = sym * (int(num) - 1)
+            st = 1
+    ires += res
+    i += st
+with open('file1.txt','w') as inf1:
+    inf1.write(ires)
